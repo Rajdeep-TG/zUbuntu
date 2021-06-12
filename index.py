9f83eef5953da3168f90699b65f66d82099b133d
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-06-12 10:47:11 +0530

    Initial Flask Commit

---
+++ index.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    res = requests.get('https://TechGeeks.pythonanywhere.com/')
+    return res.content
+
+@app.route('/<path:path>')
+def routes(path):
+    res = requests.get('http://TechGeeks.pythonanywhere.com/{}'.format(path))
+    return res.content
+
+if __name__=="__main__":
+    app.run(port=5000)
